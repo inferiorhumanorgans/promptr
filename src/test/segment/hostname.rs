56eb6b4264493e343e2f18302b930cb334b4e816
Alex Zepeda <github@inferiorhumanorgans.com>
2022-04-08 00:40:08 -0700

    Hostname: make the tests work inside a jail

--- src/test/segment/hostname.rs
+++ src/test/segment/hostname.rs
@@ -9,6 +9,12 @@ declare_segement_test!([
 
 segment_test! {
     fn hostname() {
+        let args = r##"
+            {
+                "show_jail_indicator": false
+            }
+        "##;
+
         |args, state| {
             let seg = Hostname::to_segment_generic(args, &state).unwrap();
             assert_eq!(1, seg.len());
@@ -23,7 +29,8 @@ segment_test! {
     fn hostname_with_domain() {
         let args = r##"
             {
-                "show_domain": true
+                "show_domain": true,
+                "show_jail_indicator": false
             }
         "##;
 
